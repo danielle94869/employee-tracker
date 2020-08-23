c88e576ce54c571cead8b4b89fb36904216b6a59
Daniel Le <lequangduc512014@gmail.com>
2020-08-23 15:11:27 -0700

    add tables to the platform

---
+++ routes/itemRoutes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router()
+const db = require('../db')
+
+// GET all items
+router.get('/items', (req, res) => {
+  db.query('SELECT * FROM items', (err, items) => {
+    if (err) { console.log(err) }
+    res.json(items)
+  })
+})
+
+// POST one item
+router.post('/items', (req, res) => {
+  db.query('INSERT INTO items SET ?', req.body, (err, fields) => {
+    if (err) { console.log(err) }
+    res.json({ id: fields.insertId })
+  })
+})
+
+// PUT one item
+router.put('/items/:id', (req, res) => {
+  db.query('UPDATE items SET isDone = ? WHERE id = ?', [req.body.isDone, req.params.id], err => {
+    if (err) { console.log(err) }
+    res.sendStatus(200)
+  })
+})
+
+// DELETE one item
+router.delete('/items/:id', (req, res) => {
+  db.query('DELETE FROM items WHERE id = ?', req.params.id, err => {
+    if (err) { console.log(err) }
+    res.sendStatus(200)
+  })
+})
+
+module.exports = router
---
+++ sql/seed.sql
@@ -0,0 +1,9 @@
+USE employee_db;
+
+INSERT INTO role (text, isDone)
+VALUES ('Take out trash', false), 
+('Cook dinner', true), 
+('Do laundry', false),
+('Walk the dog', true),
+('Pick up dry cleaning', false)
+('Mow the lawn', false);
